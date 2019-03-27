3f2cf043ca6ff533b146242e2563bb5a41d33bbf
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2019-03-28 00:53:32 +0300

    добавил php

---
+++ Sites/SM58/php/send.php
@@ -0,0 +1,15 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+echo 'One line simple string.<br />'; 
+echo $fio;
+echo "<br>";
+echo $fio;
+
+?>
