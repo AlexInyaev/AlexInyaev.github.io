b8473d52a46df8547c9de8093a37ee4683e5e982
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2022-03-29 01:32:19 +0300

    add manual folder

---
+++ TrainingManual/js/index.js
@@ -0,0 +1,12 @@
+let div = document.createElement('div');
+div.className = "map-themes";
+div.innerHTML = `
+<ol>
+<li><a href="../index.html">Главная</a></li>
+<li><a href="failes/array.html">Массивы</a></li>
+<li><a href="#">Объекты</a></li>
+<li><a href="#">Функции</a></li>
+<li><a href="#">Цикл For</a></li>
+<li><a href="#">if</a></li>
+</ol>`;
+document.body.prepend(div)
